7a422b6ccfba57ebb4f7f9da4604a5b4e5bf7b95
Mike-hope-23 <mikestefan1926@gmail.com>
2023-11-14 15:59:13 +0100

    Ran npm commans

--- src/main.js
+++ src/main.js
@@ -2,23 +2,23 @@
 function Solve(val) {
     var v = document.getElementById('res');
     v.value += val;
- }
- function Result() {
+}
+function Result() {
     var num1 = document.getElementById('res').value;
     var num2 = eval(num1);
     document.getElementById('res').value = num2;
- }
+}
 
- /* Clear all */
+/* Clear all */
 
- function Clear() {
+function Clear() {
     var inp = document.getElementById('res');
     inp.value = '';
- }
+}
 
- /* Clear last entry */
+/* Clear last entry */
 
- function Back() {
+function Back() {
     var ev = document.getElementById('res');
-    ev.value = ev.value.slice(0,-1);
- }
+    ev.value = ev.value.slice(0, -1);
+}
